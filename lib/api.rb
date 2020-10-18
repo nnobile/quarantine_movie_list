aa4ad7a745b9526c647122de47ca6bded705d0ce
Nick Nobile <nickcnobile@gmail.com>
2020-10-18 18:55:19 +0000

    Further built out API class

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,18 @@
 class API
 
+  def self.retrieve_movies
+    url = #placeholder URL
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    hash = JSON.parse(reponse)
+
+    array_of_movies = hash["movies"]
+
+    array_of_movies.each do |movies_hash|
+      movie = Movie.new
+      movie.name = movie_hash[]
+      movie.rating = movie_hash[]
+    end
+  end
+  
 end
