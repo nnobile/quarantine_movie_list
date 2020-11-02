2756bb9c34b410f3fa9f943379151cb5bfb91867
Nick Nobile <nickcnobile@gmail.com>
2020-11-02 02:22:45 +0000

    Added gitignore

--- lib/api.rb
+++ lib/api.rb
@@ -3,7 +3,9 @@ require 'pry'
 class API
 
   def self.retrieve_movies
-    url = "https://api.themoviedb.org/3/discover/movie?api_key=3cb858a27ba1fcb6f33f21f11e03295a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false"
+    api_key = ENV['API_KEY']
+
+    url = "https://api.themoviedb.org/3/discover/movie?api_key=#{api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     films_hash = JSON.parse(response)
