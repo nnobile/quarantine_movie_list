f86b7615c5acc32420569bbc7d74cb11e7a0592b
Nick Nobile <nickcnobile@gmail.com>
2020-11-03 20:00:15 +0000

    Updated .env file

--- lib/api.rb
+++ lib/api.rb
@@ -4,7 +4,6 @@ class API
 
   def self.retrieve_movies
     api_key = ENV['API_KEY']
-
     url = "https://api.themoviedb.org/3/discover/movie?api_key=#{api_key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false"
     uri = URI(url)
     response = Net::HTTP.get(uri)
